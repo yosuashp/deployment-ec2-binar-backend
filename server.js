027445faf35f623e334b83b7744d7efeda9a8006
yosuashp <sahatparulian85@gmail.com>
2023-12-25 23:49:02 +0700

    update cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require('dotenv').config({ path: envPath })
 const app = express()
 
 app.use(cors({
-  origin: 'https://front-end-binar-aov4zrr5r-yosuas-projects-8e558ba3.vercel.app'
+  origin: 'https://front-end-binar.vercel.app/'
 }))
 
 // create a write stream (in append mode)
